72fbb3e1c5f4ea798f10dfab8efee2b31184ff0a
rarias <riqarias@gmail.com>
2019-09-30 15:08:25 -0300

    Corregido

--- Proyectos_asm/filtro.asm
+++ Proyectos_asm/filtro.asm
@@ -8,7 +8,7 @@
 .include "m328def.inc"
 
 .EQU CI = 0
-.EQU LONG_FIL = 6
+.EQU LONG_FIL = 6	; no! E(k), S(k) y S(k+1) son variables de 8 bits alojadas en RAM, es decir, 1 byte (no 6)
 
 .DSEG
 	S: .BYTE LONG_FIL
@@ -17,6 +17,7 @@
 .CSEG 
 	RJMP MAIN
 
+.ORG	INT_VECTORS_SIZE	; Saltear los vectores de interrupción
 MAIN:	
 	LDI XL, LOW(S)
 	LDI XH, HIGH(S)
@@ -24,26 +25,26 @@ MAIN:
 	LDI YH, HIGH(E)
 	LDI R20, LONG_FIL
 	CALL FILTRO
+	
+; acá faltaría un while(1);
 
 
 FILTRO:
-	LD R16, X
-	LD R17, Y+
+	LD R16, X	; S
+	LD R17, Y+	; E
 	LDI R18, 3
 
 	ASR R16
-	ASR R16
+	ASR R16		; S/4
 	ASR R17
-	ASR R17
-	MULS R17, R18
-	MOV R17, R0
+	ASR R17		; E/4
+	MULS R17, R18	; error! este micro no tiene MULS, sólo MUL
+	MOV R17, R0	; se puede mejorar el error de la operación si primero se multiplica x 3 y luego se shiftean r1 y r0 2 veces
 	ADD R16, R17
 	
 	ST X+, R16 
-	
-	DEC R20
-	CPI R20, 0
-	BRNE FILTRO
-RET
 
-	
+	DEC R20		; no va
+	CPI R20, 0	; no va
+	BRNE FILTRO	; no va
+	RET
