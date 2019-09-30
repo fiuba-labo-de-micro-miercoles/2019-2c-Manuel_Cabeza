e36e9eb508dd9340d5bb520d41e7d86365b05a44
rarias <riqarias@gmail.com>
2019-09-30 15:25:02 -0300

    corregido

--- Proyectos_asm/ordenar_vector.asm
+++ Proyectos_asm/ordenar_vector.asm
@@ -27,9 +27,10 @@ ORDENAR:
 	LD R17, X+
 	ST Y+, R17
 	DEC R20
-	CPI R20, 0
-	BREQ INICIALIZAR
-	RJMP ORDENAR
+	;CPI R20, 0		; línea comentada
+	;BREQ INICIALIZAR	; línea comentada
+	;RJMP ORDENAR		; línea comentada
+	brne	ORDENAR		; es más simple
 
 INICIALIZAR:
 	LDI YL, LOW(VEC2)
@@ -41,19 +42,20 @@ BURBUJEO:
 	LD R18, Y
 	
 	DEC R20
-	CPI R20, 0
+	;CPI R20, 0	; de más, DEC te deja el flag Z calculado
 	BREQ VOLVER
 
 	CP R18, R17
 	BRSH BURBUJEO
 	
-	MOV R21, R17
-	MOV R17, R18
-	MOV R18, R21
+	; MOV R21, R17
+	; MOV R17, R18
+	; MOV R18, R21
+	;ST Y , R18
+	;ST -Y , R17
 	
-	ST Y , R18
-	ST -Y , R17
-	
+	st	Y, r17	; estas 2 líneas reemplazan a las 5 anteriores
+	st 	-Y, r18
 	RJMP BURBUJEO
 
 
@@ -71,10 +73,10 @@ VERIFICAR:
 	BRLO INICIALIZAR
 
 	DEC R20
-	CPI R20, 0
-	BREQ TERMINAR
-	RJMP VERIFICAR
+	; CPI R20, 0
+	; BREQ TERMINAR
+	; RJMP VERIFICAR
+	brne	VERIFICAR	; reemplaza a las 3 líneas anteriores
 
-TERMINAR: RET
-
-
+TERMINAR: 
+	RET
